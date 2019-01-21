276e4696dc11a2c10887f9d6c50efe609ff7fc7d
Miika <miika.roiha@gmail.com>
2019-01-21 20:49:56 +0200

    unicafetehtava

--- osa01/unicafe/src/index.js
+++ osa01/unicafe/src/index.js
@@ -5,36 +5,83 @@ const Otsikko = ()=>{
         <h1>anna palautetta</h1>
     )
 }
-const Nappulat = () => {
+const Nappulat = ({handleGood, handleNeutral, handleBad}) => {
     return (
         <span>
-            <Button /> 
-            <Button /> 
-            <Button />
+            <Button teksti="hyvä" handleClick={handleGood} /> 
+            <Button teksti="neutraali" handleClick={handleNeutral}/> 
+            <Button teksti="huono" handleClick={handleBad}/>
         </span>
     )
 }
 
 
-const Button = () => {
+const Button = ({teksti,handleClick}) => {
     return (
         <span>
-            <button>
-                teksti
+            <button onClick={handleClick}>
+                {teksti}
             </button>
         </span>
     )
 }
 
-const Tilastot = () => {
-    return(
-        <div>
-            <h1>statistiikka</h1>
-            <p>hyvä </p>
-            <p>neutraali </p>
-            <p>huono </p>
-        </div>
-    )
+const Statistics = ({good, neutral, bad}) => {
+
+    let summa = good + neutral + bad
+    let keskiarvo = (1*good -1*bad + 0*neutral)/summa
+    let positiivisia = ((good/summa)*100)
+
+    if(summa === 0){
+        return(
+            <p>Ei yhtään palautetta annettu
+                
+
+
+
+            </p>
+            
+        )
+    } else {
+        return(
+            <div>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td><h1>Statistiikka</h1></td>   
+                    </tr>
+                    <tr>
+                        <td>hyvä</td>
+                        <td>{good}</td>
+                    </tr>
+                    <tr>
+                        <td>neutraali</td>
+                        <td>{neutral}</td>
+                    </tr>
+                    <tr>
+                        <td>huono</td>
+                        <td>{bad}</td>
+                    </tr>
+                    <tr>
+                        <td>yhteensä</td>
+                        <td>{summa}</td>
+                    </tr>
+                    <tr>
+                        <td>keskiarvo</td>
+                        <td>{keskiarvo}</td>
+                    </tr>
+                    <tr>
+                        <td>positiivisia</td>
+                        <td>{positiivisia} %</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        )
+    }
+
+    
 }
 
 
@@ -44,16 +91,21 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const plusGood = () =>{
+        setGood(good+1)
+    }
+    const handleNeutral = () =>{
+        setNeutral(neutral+1)
+    }
+    const handleBad = () =>{
+        setBad(bad+1)
+    }
+
     return (
         <div>
-
             <Otsikko/>
-            <Nappulat/>
-            <Tilastot/>
-
-
-
-
+            <Nappulat handleGood={plusGood} handleNeutral={handleNeutral} handleBad={handleBad}/>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     )
 }
